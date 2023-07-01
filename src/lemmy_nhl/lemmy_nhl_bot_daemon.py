f98116cc9254276fad614399e9434bb55144da72
Michael Bell <tuc03516@gmail.com>
2023-07-01 15:08:36 -0600

    clean up duplicate of function

--- src/lemmy_nhl/lemmy_nhl_bot_daemon.py
+++ src/lemmy_nhl/lemmy_nhl_bot_daemon.py
@@ -15,7 +15,7 @@ stats = False
 postID = 0
 teamID = 0
 newSchedule = False
-
+srv = ""
 # create time based services
 # check for game today:
 
@@ -42,7 +42,7 @@ def isGame():
 
 #create post for linescore
 def create_post_linescore():
-    global teamID, gamePK, isMod
+    global teamID, gamePK, isMod, srv, postID
     #get team Names/date/regular Season
     game_today = "https://statsapi.web.nhl.com/api/v1/game/" + str(gamePK) + "/linescore"
     game_today2 = "https://statsapi.web.nhl.com/api/v1/schedule?teamId="
@@ -71,7 +71,7 @@ def create_post_linescore():
 
 #Linescore for game
 def gameTime():
-    global gameOver, postID, gamePK
+    global gameOver, postID, gamePK, srv
     body2, gameOver = post_body.post_body_linescore(gamePK)
     try:
         test = srv.edit_post(postID, body=body2)
@@ -82,43 +82,9 @@ def gameTime():
 
 
 #get community ID
-def get_communityID(communityName):
-    request = srv.get_community(None, communityName)
-    global CID
-    CID = request.json().get("community_view")
-    CID = CID["community"].get("id")
-
-
-#create post_linescore
-def create_post_linescore():
-    global teamID, gamePK, isMod
-    #get team Names/date/regular Season
-    game_today = "https://statsapi.web.nhl.com/api/v1/game/" + str(gamePK) + "/linescore"
-    game_today2 = "https://statsapi.web.nhl.com/api/v1/schedule?teamId="
-    game_today2 = game_today + str(teamID)
-    r = requests.get(game_today)
-    t = requests.get(game_today2)
-    home_name = r.json().get("teams").get("home").get("team").get("name")
-    away_name = r.json().get("teams").get("away").get("team").get("name")
-    date = t.json().get("dates")[0].get("date")
-    gameType = t.json().get("dates")[0].get("games")[0].get("gameType")
-    if(gameType == "R"):
-        gameType = "Regular Season"
-    elif(gameType == "PR"):
-        gameType = "Pre-Season"
-    else:
-        gameType = "Playoffs"
-    postName = away_name + " vs. " + home_name + " " + gameType + " " + str(date)
-    global CID, postID
-    temp = CID
-    post = srv.create_post(temp, postName, body="")
-    postID = post.json().get("post_view")
-    postID = postID["post"].get("id")
-    if(isMod == "y"):
-        srv.feature_post("Community", True, postID)
 
 def create_post_stats():
-    global CID, teamID
+    global CID, teamID, srv
     today = time.strftime("%m-%d-%Y")
     title = "Team Stats for the Season as of " + today
     body = post_body.post_body_stats(teamID)
@@ -132,90 +98,95 @@ def create_post_standings():
     post = srv.create_post(CID, postName, body=body)
 
 #main loop segment
-dbLocation = os.path.expanduser("~/.cache/lnhl.db")
-if(os.path.exists(dbLocation) == False):
-    print("Please run config.py first!")
-    sys.exit()
+def daemon():
+    global token, communityName, server, teamID, isMod, games, standings, stats, post, CID, newSchedule, gameOver, srv
+    dbLocation = os.path.expanduser("~/.cache/lnhl.db")
+    if(os.path.exists(dbLocation) == False):
+        print("Please run config.py first!")
+        sys.exit()
 
-#sql database connection/data grabbing
-dbLocation = os.path.expanduser("~/.cache/lnhl.db")
-con = sqlite3.connect(dbLocation)
-cur = con.cursor()
-#get login token
-r = cur.execute("SELECT token FROM user")
-temp = r.fetchall()
-token = str(temp[0])
-token = token.lstrip("('")
-token = token.rstrip("',)")
-#get community Name
-r = cur.execute("SELECT communityName FROM user")
-temp = r.fetchall()
-communityName = str(temp[0])
-communityName = communityName.lstrip("('")
-communityName = communityName.rstrip("',)")
-# get server name
-r = cur.execute("SELECT server FROM user")
-temp = r.fetchall()
-server = str(temp[0])
-server = server.lstrip("('")
-server = server.rstrip("',)")
-#get teamID
-r = cur.execute("SELECT teamID FROM user")
-temp = r.fetchall()
-teamID = str(temp[0])
-teamID = teamID.lstrip("('")
-teamID = teamID.rstrip("',)")
+    #sql database connection/data grabbing
+    dbLocation = os.path.expanduser("~/.cache/lnhl.db")
+    con = sqlite3.connect(dbLocation)
+    cur = con.cursor()
+    #get login token
+    r = cur.execute("SELECT token FROM user")
+    temp = r.fetchall()
+    token = str(temp[0])
+    token = token.lstrip("('")
+    token = token.rstrip("',)")
+    #get community Name
+    r = cur.execute("SELECT communityName FROM user")
+    temp = r.fetchall()
+    communityName = str(temp[0])
+    communityName = communityName.lstrip("('")
+    communityName = communityName.rstrip("',)")
+    # get server name
+    r = cur.execute("SELECT server FROM user")
+    temp = r.fetchall()
+    server = str(temp[0])
+    server = server.lstrip("('")
+    server = server.rstrip("',)")
+    #get teamID
+    r = cur.execute("SELECT teamID FROM user")
+    temp = r.fetchall()
+    teamID = str(temp[0])
+    teamID = teamID.lstrip("('")
+    teamID = teamID.rstrip("',)")
 
-#get mod status
-r = cur.execute("SELECT isMod FROM user")
-temp = r.fetchall()
-isMod = str(temp[0])
-isMod = isMod.lstrip("('")
-isMod = isMod.rstrip("',)")
+    #get mod status
+    r = cur.execute("SELECT isMod FROM user")
+    temp = r.fetchall()
+    isMod = str(temp[0])
+    isMod = isMod.lstrip("('")
+    isMod = isMod.rstrip("',)")
 
-#get games
-r = cur.execute("SELECT * FROM schedule")
-games = r.fetchall()
+    #get games
+    r = cur.execute("SELECT * FROM schedule")
+    games = r.fetchall()
 
-#use login token, check for game, get community id, create post, then loop
-srv = LemmyHttp(server)
-srv.key = token
-get_communityID(communityName)
+    #use login token, check for game, get community id, create post, then loop
+    srv = LemmyHttp(server)
+    srv.key = token
+    request = srv.get_community(None, communityName)
+    global CID
+    CID = request.json().get("community_view")
+    CID = CID["community"].get("id")
 
 
-#main loop
-while(True):
-    isGame()
-    today = time.strftime("%a")
-    if(str(today) == "Mon" and standings == False):
-        create_post_standings()
-        create_post_stats()
-        standings = True
-        stats = True
-    elif(str(today) == "Tue" and standings == True):
-        standings = False
-        stats = False
-    #check for new schedule in August, re-run schedule to database
-    month = int(time.strftime("%m"))
-    if(month == 8 and newSchedule == False):
-        newSchedule = True
-        cur.execute("DELETE FROM schedule")
-        con.commit()
-        year = int(time.strftime("%Y"))
-        t = requests.get("https://statsapi.web.nhl.com/api/v1/schedule?teamId=" + str(teamID) + "&season=" + str(today) + str(year))
-        games = t.json().get("dates")
-        i = len(games)
-        j = 0
-        while(j < i):
-            gamePK = games[j].get("games")[0].get("gamePk")
-            date = games[j].get("games")[0].get("gameDate")
-            date = date.split("T")
-            date[1] = date[1].rstrip("Z")
-            cur.execute("INSERT INTO schedule VALUES (?, ?, ?);", (gamePK, date[0], date[1]))
+    #main loop
+    while(True):
+        isGame()
+        today = time.strftime("%a")
+        if(str(today) == "Sat" and standings == False):
+            create_post_standings()
+            create_post_stats()
+            standings = True
+            stats = True
+        elif(str(today) == "Tue" and standings == True):
+            standings = False
+            stats = False
+        #check for new schedule in August, re-run schedule to database
+        month = int(time.strftime("%m"))
+        if(month == 8 and newSchedule == False):
+            newSchedule = True
+            cur.execute("DELETE FROM schedule")
             con.commit()
-            j = j + 1
-        r = cur.execute("SELECT * FROM schedule")
-        games = r.fetchall()
-    elif(month == 7):
-        newSchedule = False
-    time.sleep(300)
+            year = int(time.strftime("%Y"))
+            t = requests.get("https://statsapi.web.nhl.com/api/v1/schedule?teamId=" + str(teamID) + "&season=" + str(today) + str(year))
+            games = t.json().get("dates")
+            i = len(games)
+            j = 0
+            while(j < i):
+                gamePK = games[j].get("games")[0].get("gamePk")
+                date = games[j].get("games")[0].get("gameDate")
+                date = date.split("T")
+                date[1] = date[1].rstrip("Z")
+                cur.execute("INSERT INTO schedule VALUES (?, ?, ?);", (gamePK, date[0], date[1]))
+                con.commit()
+                j = j + 1
+            r = cur.execute("SELECT * FROM schedule")
+            games = r.fetchall()
+        elif(month == 7):
+            newSchedule = False
+        time.sleep(300)
