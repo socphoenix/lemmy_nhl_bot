29bb5e82cb86e178cb7f7848916aba1ea759c867
socphoenix <tuc03516@gmail.com>
2023-07-02 13:24:26 -0600

    undo changes meant for docker branch database location

--- src/lemmy_nhl/lemmy_nhl_bot_daemon.py
+++ src/lemmy_nhl/lemmy_nhl_bot_daemon.py
@@ -126,13 +126,12 @@ def create_post_standings():
 def daemon():
     global token, communityName, server, teamID, isMod, games, standings, stats, post, CID, newSchedule, gameOver, srv
     dbLocation = os.path.expanduser("~/.cache/lnhl.db")
-    if(os.path.exists("/opt/lnhl.db") == False):
+    if(os.path.exists(dbLocation) == False):
         print("Please run config.py first!")
         sys.exit()
 
     #sql database connection/data grabbing
-    dbLocation = os.path.expanduser("/lib/python3.11/site-packages/lemmy_nhl/lnhl.db")
-    con = sqlite3.connect("/opt/lnhl.db")
+    con = sqlite3.connect(dbLocation)
     cur = con.cursor()
     #get login token
     r = cur.execute("SELECT token FROM user")
