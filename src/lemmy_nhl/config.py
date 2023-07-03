a230e11d51edaf1f027103bc4b1f75597b7a2b6d
socphoenix <tuc03516@gmail.com>
2023-07-02 20:42:02 -0600

    Update config.py

--- src/lemmy_nhl/config.py
+++ src/lemmy_nhl/config.py
@@ -12,10 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/docker
 #basic config, will save the token key for future logins, team, community name
 import getpass
 from plemmy import LemmyHttp
